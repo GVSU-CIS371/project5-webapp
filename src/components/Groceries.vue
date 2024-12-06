dacbe48acfd0c36cc2b7e5c46d783478cd45926a
Joshua Cooling <cooljosh097@gmail.com>
2024-12-06 14:55:32 -0500

    Added files from project 4

--- src/components/Groceries.vue
+++ src/components/Groceries.vue
@@ -1,7 +1,19 @@
 <template>
-  <!-- your answer -->
+  <v-container>
+    <v-row>
+      <v-col v-for="product in filteredProducts" :key="product.id" cols="12" md="4" lg="3">
+        <StoreItem :product="product.data" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script lang="ts" setup>
-// your answer
+<script setup lang="ts">
+import { useProductStore } from "../stores/ProductStore";
+import StoreItem from "../components/StoreItem.vue";
+
+const productStore = useProductStore();
+
+const filteredProducts = productStore.filterByCategory("Groceries");
 </script>
+
