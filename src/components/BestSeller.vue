f4b2141e0f391859337c8630e6c6735cff9ec3b3
Joshua Cooling <cooljosh097@gmail.com>
2024-12-06 05:27:07 -0500

    added code from project 4

--- src/components/BestSeller.vue
+++ src/components/BestSeller.vue
@@ -1,7 +1,17 @@
 <template>
-  <!-- your answer -->
+  <v-container>
+    <v-row>
+      <v-col v-for="product in filteredProducts" :key="product.id" cols="12" md="4" lg="3">
+        <StoreItem :product="product.data" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-// your answer
+import { useProductStore } from "../stores/ProductStore";
+import StoreItem from "../components/StoreItem.vue";
+
+const productStore = useProductStore();
+const filteredProducts = productStore.filterByRating(4.5);
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,20 @@
 <template>
-  <!-- your answer -->
+  <v-container>
+    <v-row>
+      <v-col v-for="product in products" :key="product.id" cols="12" md="4" lg="3">
+        <StoreItem :product="product.data" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script lang="ts" setup>
-// your answer
+<script setup lang="ts">
+import { useProductStore } from "../stores/ProductStore";
+import StoreItem from "./StoreItem.vue";
+
+const productStore = useProductStore();
+productStore.init();
+
+const products = productStore.products;
 </script>
+
